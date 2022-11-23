e5aa9fcc10708c6e921dd848b9239b1583948207
virtual-devs <virtual.devs2022@gmail.com>
2022-11-23 02:44:05 -0600

    created model user

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../database/database.js";
+
+export const User = sequelize.define(
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rol: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
