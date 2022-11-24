155a4fdae83da3fcc149e7bf79c3c8d769602483
virtual-devs <virtual.devs2022@gmail.com>
2022-11-23 18:17:24 -0600

    Created login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import { User } from "../models/User.js";
-import { encryptPassword } from "../models/User.js";
+import jwt from 'jsonwebtoken'
+import { SECRET_KEY } from "../config.js";
+import { encryptPassword, comparePassword } from "../models/User.js";
+
 
 export const signUp = async (req, res) => {
   try {
@@ -30,4 +33,27 @@ export const signUp = async (req, res) => {
   }
 };
 
-export const signIn = async (req, res) => {};
+export const signIn = async (req, res) => {
+    try {
+        const {email, password, rol } = req.body;
+        const userFound = await User.findOne({
+            where:{
+                email: email,
+                rol: rol
+            }
+        });
+
+        if(!userFound) return res.status(400).json({message: "Usuario no encontrado"})
+        
+        const matchPassword = await comparePassword(password, userFound.password);
+        
+        if(!matchPassword) return res.status(401).json({message: "Contraseña incorrecta"})
+        
+        const token = jwt.sign({id: userFound.id}, SECRET_KEY, {expiresIn: 86400})
+        userFound.dataValues.token = token;
+        res.json(userFound.dataValues)
+
+    } catch (error) {
+        return res.status(500).json({ massage: error.massage });
+    }
+};
